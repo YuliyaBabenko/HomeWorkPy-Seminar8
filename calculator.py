10b9eef92153ec11213bd2b3dae2ed24c2ff5b5c
YuliyaB <114241910+YuliyaBabenko@users.noreply.github.com>
2023-02-02 11:41:10 +0300

    done

---
+++ calculator.py
@@ -0,0 +1,26 @@
+import math
+  
+def cos(num):
+    return math.cos(math.radians(num))
+ 
+ 
+def sin(num):
+    return math.sin(math.radians(num))
+ 
+ 
+def tg(num):
+    return math.tan(math.radians(num))
+ 
+ 
+def ctg(num):
+    return 1 / math.tan(math.radians(num))
+ 
+ 
+def ln(num):
+    return math.log(math.radians(num))
+ 
+calc = input('Введите выражение для вычисления: ')
+ 
+calc = calc.replace("^", "**")
+ 
+print(eval(calc))
